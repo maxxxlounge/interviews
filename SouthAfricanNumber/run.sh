e1a9c4d944005721bdf62d96065f97ed2f1d8923
massimo.fornasier <massimo.fornasier@pixartprinting.com>
2021-02-10 08:29:19 +0100

    last last polish on rin.sh

--- SouthAfricanNumber/run.sh
+++ SouthAfricanNumber/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker pull golang
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest rm ./SouthAfricanNumber
 docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest go test -v ./... -cover
 docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest go build -v -o SouthAfricanNumber
 docker run -p 8888:80  --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:latest ./SouthAfricanNumber
